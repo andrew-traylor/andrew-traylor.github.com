46ddcf48f413aee3fecc45d337f92c237803f486
andrew-traylor <atraylor@eagles.bridgewater.edu>
2015-01-29 00:56:56 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,3 @@
-var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Hello world!';
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+    }
